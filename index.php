ab89a8ee4a342c318ebc1d595e5b6e77aeb44c30
David Álvarez <davidalvarezcalvo@gmail.com>
2016-10-04 15:08:25 +0200

    Tercera subida

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
+//ini_set('display_erros', 'On');error_reporting(E_ALL);phpinfo();
 require_once "vendor/autoload.php";require 'vendor/autoload.php';
+require_once "secret.php";
+
 use GuzzleHttp\Client;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-define("CLIENT_ID", "4df767750d944ea4a0025a2523f249c4");
-define("CLIENT_SECRET", "ed187ab8b6504093bd2bb4049fec8aad");
-define("REDIRECT_URL", "https://getinst.promofb.es/dos");
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
-
-
 
 $app = new \Slim\App(["settings" => $config]);
 $container = $app->getContainer();
@@ -24,48 +22,31 @@ $app->get('/', function (Request $request, Response $response) {
     $res = $client->request('GET', 'https://api.instagram.com/oauth/authorize', ['query' => [
                     'client_id' => CLIENT_ID,
                     'redirect_uri' => REDIRECT_URL,
-                    'response_type' => 'code'
+                    'response_type' => 'code',
+                    'scope' => 'public_content'
                     ]]);
-       
-       //print_r($res->getHeader('content-type')); // text/html
-       echo $res->getBody();
-    
-    $response = $this->view->render($response, "index.phtml", ["tickets" => ""]);
-    return $response;
+    if($res->getStatusCode()=='200') {
+        echo $res->getBody();
+    } else {
+        return $this->view->render($response, "index.phtml", ["tickets" => ""]);
+    }
 });
+
 
 // Despues de loguearme en Instagram en devuelve aqui con el codigo  http://your-redirect-uri?code=CODE
 $app->get('/dos', function (Request $request, Response $response) {
-    echo "CODE".$app->request->get('code'); die;
+
+   if($request->getAttribute('erro')){
+        $response = $this->view->render($response, "index.phtml", ["error" => $request->getAttribute('error_reason') ]);
+   }
+   $access_token = $request->getAttribute('access_token');
+   echo "333".$code = $request->getAttribute('code'); die;
     
-    /* 
-
-    if($app->request->get('code')){
-
-        $code = $app->request->get('code');
-
-        $response = $client->post('https://api.instagram.com/oauth/authorize', array('body' => array(
-            'client_id' => CLIENT_ID,
-            'redirect_uri' => REDIRECT_URL,
-            'response_type' => 'code'
-        )));
-
-        $data = $response->json();
-
-    }else{
-
-        $login_url = "https://api.instagram.com/oauth/authorize?client_id={$client_id}&redirect_uri={$redirect_url}&scope=basic&response_type=code";
-
-    }
-
-
-    $app->render('home.php', array('data' => $data, 'login_url' => $login_url));*/
-    
-    $response = $this->view->render($response, "index.phtml", ["tickets" => ""]);
-    return $response;
+   $response = $this->view->render($response, "index.phtml");
+   return $response;
 });
 
-
+/*
 $app->post('/busca', function (Request $request, Response $response) {
     $data = $request->getParsedBody();
     $dataa = [];
@@ -76,11 +57,6 @@ $app->post('/busca', function (Request $request, Response $response) {
 
     return $response;
 });
-$app->run();
-/*
-$loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader, array(
-  //  'cache' => 'templates/cache',
-));
+*/
 
-echo $twig->render('index.twig', array('nombre' => ''));*/
+$app->run();
